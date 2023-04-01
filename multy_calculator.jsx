ecf652fb4cb4f86ccb7f14421064e1287a44b005
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 20:44:19 +0300

    Create multy_calculator.jsx

---
+++ multy_calculator.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+ const [first, updateFirst] = useState(1);
+ const [second, updateSecond] = useState(1);
+ const [multiple, multiply] = useState(0);
+
+ useEffect(() => {
+  multiply(first * second);
+ }, [, ]);
+
+ return (
+  <div>
+   <h1>Multiplier Calculator</h1>
+   <input
+    type="number"
+    value={first}
+    onChange={(e) => {
+     updateFirst(e.target.value);
+    }}
+    placeholder="The first number..."
+   />
+   <p>multiplied by:</p>
+   <input
+    type="number"
+    value={second}
+    onChange={(e) => {
+     updateSecond(e.target.value);
+    }}
+    placeholder="The second number..."
+   />
+   <p>Equals:</p>
+   <h3>{multiple}</h3>
+  </div>
+ );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
